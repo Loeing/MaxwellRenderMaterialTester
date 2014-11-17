95257057dd2fb29d3a6e895d93c3df4c3b9adef6
Joshua Tang <jlt81@case.edu>
2014-11-17 17:05:46 -0500

    Stopped hard coding file location

--- MaterialsTester.py
+++ MaterialsTester.py
@@ -4,7 +4,7 @@ import os
 def run_maxwell_render():
 	parameters = []
 	
-	parameters.append('-mxs:C:\Users\Joshua\workspace\MaxwellScenes\Test.mxs')
+	parameters.append('-mxs:' + destpath)
 	parameters.append('-res:300x200')
 	parameters.append('-time:10')
 	parameters.append('-sl:5')
@@ -12,32 +12,24 @@ def run_maxwell_render():
 	runMaxwell(parameters)
 	return 1
 
-
+scenePath = 'C:\Users\Joshua\Documents\MatScene\standardmattest.mxs'
+matPath = 'C:\Program Files\Next Limit\Maxwell 3\materials database\mxm files\\'
 scene = Cmaxwell(mwcallback)
-scene.readMXS('C:\Users\Joshua\Documents\MatScene\standardmattest.mxs')
+scene.readMXS(scenePath)
 mainobject = scene.getObject('Material (Main)')
 ringobject = scene.getObject('Material (Ring)')
 i = 1
-matPath = 'C:\Program Files\Next Limit\Maxwell 3\materials database\mxm files\\'
-for filename in os.listdir('C:\Program Files\Next Limit\Maxwell 3\materials database\mxm files'):
+for filename in os.listdir(matPath):
 	print(matPath + filename)
-	#scene.addMaterial(material.read(filename))
 	material = scene.createMaterial("TestedMaterial", True)
 	layer = material.addLayer()
 	bsdf = layer.addBSDF()
-	print(material.read(matPath + filename))	  
-	#print(CMaxwellMaterial.getMxmInfo(filename))
-	print(material.getName())
-	print(mainobject.setMaterial(material))
-	print(ringobject.setMaterial(material))
-	path = "C:\Users\Joshua\Documents\GitHub\MaxwellRenderMaterialTester\Test\{0}Test.mxs".format(i)
- 	scene.writeMXS(path)
-	i += 1
-	#run_maxwell_render()
-	
-
-
-
-
-
-
+	ok = material.read(matPath + filename)
+	print(ok)
+	if ok == 1:
+		print(material.getName())
+		print(mainobject.setMaterial(material))
+		print(ringobject.setMaterial(material))
+		destPath = "C:\Users\Joshua\Documents\MatScene\{0}Test.mxs".format(i)
+ 		scene.writeMXS(destPath)
+		i += 1
