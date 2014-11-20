9dbac3eefb61fde666861c0840c64f35d5cbe631
Joshua Tang <jlt81@case.edu>
2014-11-19 22:52:28 -0500

    does multiple sampling levels in a naive way

--- MaterialsTester.py
+++ MaterialsTester.py
@@ -1,4 +1,5 @@
 from pymaxwell import *
+from datetime import datetime
 import os
 
 def run_maxwell_render():
@@ -7,7 +8,7 @@ def run_maxwell_render():
 	parameters.append('-mxs:' + destpath)
 	parameters.append('-res:300x200')
 	parameters.append('-time:10')
-	parameters.append('-sl:5')
+	parameters.append('-sl:{0}'.format(sl))
 	
 	runMaxwell(parameters)
 	return 1
@@ -19,6 +20,8 @@ scene.readMXS(scenePath)
 mainobject = scene.getObject('Material (Main)')
 ringobject = scene.getObject('Material (Ring)')
 i = 1
+slList =  [6,8,10,12,14,16,18,20,22,24,26,28,30]
+f = open('workfile', 'w')
 for filename in os.listdir(matPath):
 	print(matPath + filename)
 	material = scene.createMaterial("TestedMaterial", True)
@@ -30,6 +33,12 @@ for filename in os.listdir(matPath):
 		print(material.getName())
 		print(mainobject.setMaterial(material))
 		print(ringobject.setMaterial(material))
-		destPath = "C:\Users\Joshua\Documents\MatScene\{0}Test.mxs".format(i)
- 		scene.writeMXS(destPath)
+		for sl in slList:
+			destPath = "C:\Users\Joshua\Documents\MatScene\{0}{1}Test.mxs".format(filename,sl)
+ 			scene.writeMXS(destPath)
+			begin = datetime.now()
+			run_maxwell_render()
+			time = datetime.now() - begin
+			#f.print("Material:{0}{}")
 		i += 1
+f.close()
