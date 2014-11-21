c13c0b23afa9287eb5c9923e0f3a2ffaa1fe82a4
Joshua Tang <jlt81@case.edu>
2014-11-20 21:05:42 -0500

    ready for hpc

--- MaterialsTester.py
+++ MaterialsTester.py
@@ -16,13 +16,13 @@ def run_maxwell_render():
 scenePath = 'C:\Users\Joshua\Documents\MatScene\standardmattest.mxs'
 matPath = 'C:\Program Files\Next Limit\Maxwell 3\materials database\mxm files\\'
 scene = Cmaxwell(mwcallback)
-scene.readMXS(scenePath)
-mainobject = scene.getObject('Material (Main)')
-ringobject = scene.getObject('Material (Ring)')
-i = 1
-slList =  [6,8,10,12,14,16,18,20,22,24,26,28,30]
-f = open('workfile', 'w')
 for filename in os.listdir(matPath):
+	scene.readMXS(scenePath)
+	mainobject = scene.getObject('Material (Main)')
+	ringobject = scene.getObject('Material (Ring)')
+	i = 1
+	slList =  [6,8,10,12,14,16,18,20,22,24,26,28,30]
+	f = open('workfile', 'w')
 	print(matPath + filename)
 	material = scene.createMaterial("TestedMaterial", True)
 	layer = material.addLayer()
@@ -39,6 +39,7 @@ for filename in os.listdir(matPath):
 			begin = datetime.now()
 			run_maxwell_render()
 			time = datetime.now() - begin
-			#f.print("Material:{0}{}")
+			f.print("Material:{0} 	sl:{1} 	time:{2}ms\n".format(filename,sl, time))
+			scene.freeScene()
 		i += 1
 f.close()
