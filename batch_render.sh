542d12ccbc081b552650327039cb70a509317207
jxc761 <Jing@nplab2.EECS.CWRU.Edu>
2014-11-21 13:54:54 -0500

    fix the bugs in batch_render.sh and render_one_file.sh

--- batch_render.sh
+++ batch_render.sh
@@ -6,12 +6,13 @@ ROOT_OUTPUT_DIR=$3
 
 rm -r "$ROOT_OUTPUT_DIR"
 mkdir "$ROOT_OUTPUT_DIR"
+module load maxwell
 
-for TEST_ID in $( seq 0 $NUMB_TEST) # iterate all frame id 
+for TEST_ID in $( seq 1 $NUMB_TEST) # iterate all frame id 
 do 
   name="$TEST_ID"
-  CURRENT_MXS_PATH="${MXS_DIR}/TEST${TEST_ID}.mxs"
-  CURRENT_OUTPUT_DIR="$ROOT_OUTPUT_DIR/TEST${TEST_ID}"
+  CURRENT_MXS_PATH="${MXS_DIR}/${TEST_ID}Test.mxs"
+  CURRENT_OUTPUT_DIR="$ROOT_OUTPUT_DIR/${TEST_ID}TEST"
   mkdir "$CURRENT_OUTPUT_DIR"
-  qsub -N "${name}"  -l walltime=23:00:00 -l nodes=1:ppn=1 -v MXS_PATH=$CURRENT_MXS_PATH OUTPUT_DIR=$CURRENT_OUTPUT_DIR ./render_one_file.sh
+  qsub -N "${name}"  -l walltime=23:00:00 -l nodes=1:ppn=1 -v MXS_PATH=$CURRENT_MXS_PATH,OUTPUT_DIR=$CURRENT_OUTPUT_DIR ./render_one_file.sh
 done
--- render_one_file.sh
+++ render_one_file.sh
@@ -6,14 +6,13 @@ echo $OUTPUT_DIR
 
 #loading the maxwell module
 module load maxwell
-
 #final sampling level
 FINAL_SL_LEVEL=5
 for SL in $( seq 1 $FINAL_SL_LEVEL) 
 do
 	START=$(date +%s)
-	time maxwell -nogui -nowait -verbose:0 -trytoresume -motionblur:off -mxs:"${MXS_PATH}" -res:64x64 -sl:$SL -o:"${OUTPUT_DIR}/${SL}.png" >> "$OUTPUT_DIR/times.txt"
+	 maxwell -nogui -nowait -trytoresume -mxs:"${MXS_PATH}" -res:128x128 -sl:$SL -o:"${OUTPUT_DIR}/${SL}.png"
 	END=$(date +%s)
   DIFF=$(( $END - $START ))
-  echo $DIFF >> "$OUTPUT_DIR/record.txt"
+  echo $SL:$DIFF >> "$OUTPUT_DIR/record.txt"
 done
