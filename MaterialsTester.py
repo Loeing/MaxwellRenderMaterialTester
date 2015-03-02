d58759d917cce5e8eafbd547779477c3d65e185a
Joshua Tang <jlt81@case.edu>
2015-03-02 16:18:35 -0500

    minor changes to the python scripts

--- MaterialsTester.py
+++ MaterialsTester.py
@@ -4,15 +4,12 @@ import os
 
 def run_maxwell_render():
 	parameters = []
-	
 	parameters.append('-mxs:' + destpath)
 	parameters.append('-res:300x200')
 	parameters.append('-time:10')
 	parameters.append('-sl:{0}'.format(sl))
-	
 	runMaxwell(parameters)
 	return 1
-
 scenePath = 'C:\Users\Joshua\Documents\MatScene\standardmattest.mxs'
 matPath = 'C:\Program Files\Next Limit\Maxwell 3\materials database\mxm files\\'
 scene = Cmaxwell(mwcallback)
@@ -23,18 +20,18 @@ for filename in os.listdir(matPath):
 	i = 1
 	slList =  [6,8,10,12,14,16,18,20,22,24,26,28,30]
 	#f = open('workfile', 'w')
-	print(matPath + filename)
+	print(filename)
 	material = scene.createMaterial("TestedMaterial", True)
 	layer = material.addLayer()
 	bsdf = layer.addBSDF()
 	ok = material.read(matPath + filename)
 	print(ok)
 	if ok == 1:
-		print(material.getName())
-		print(mainobject.setMaterial(material))
-		print(ringobject.setMaterial(material))
+		material.getName()
+		mainobject.setMaterial(material)
+		ringobject.setMaterial(material)
+		destPath = "C:\Users\Joshua\Documents\Scenes\{0}Test.mxs".format(i)
 		for sl in slList:
-			destPath = "C:\Users\Joshua\Documents\Scenes\{0}Test.mxs".format(i)
  			scene.writeMXS(destPath)
 			begin = datetime.now()
 			run_maxwell_render()
